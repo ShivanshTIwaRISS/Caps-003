ab4a2c58d21f2cc9a823252c060dd328a929967e
ShivanshTIwaRISS <shivansh092322120@gmail.com>
2025-11-12 16:27:15 +0530

    2nd

--- vite/src/constants.js
+++ vite/src/constants.js
@@ -2,7 +2,7 @@ let ENDPOINT="";
 
 // eslint-disable-next-line no-undef
 if(process.env.NODE_ENV==="production"){
-    ENDPOINT='https://caps-3-2xfo-llbeiqc7h-shivanshtiwariss-projects.vercel.app';
+    ENDPOINT='https://caps-003-nwqv-gwy4ood8h-shivanshtiwariss-projects.vercel.app';
 }
 // eslint-disable-next-line no-undef
 else if(process.env.NODE_ENV==="development"){
