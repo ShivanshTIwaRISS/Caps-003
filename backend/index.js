109bf8ea26c35529c6528004af491335f65f5559
ShivanshTIwaRISS <shivansh092322120@gmail.com>
2025-11-28 03:42:17 +0530

    DONE-DONE

--- backend/index.js
+++ backend/index.js
@@ -17,28 +17,27 @@ const app = express();
 
 const allowedOrigins = [
   "https://caps-003.vercel.app",
-  "http://localhost:5173"
+  "http://localhost:5173",
 ];
 
-app.use((req, res, next) => {
-  const origin = req.headers.origin;
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 
-  if (allowedOrigins.includes(origin)) {
-    res.header("Access-Control-Allow-Origin", origin);
-  }
-
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-
-  if (req.method === "OPTIONS") {
-    return res.sendStatus(200); // IMPORTANT for preflight
-  }
-
-  next();
-});
-
+app.options("*", cors());
 app.use(express.json());
+
 
 app.get("/", (req, res) => {
   res.send("Backend is running successfully.");
--- vite/src/services/api.js
+++ vite/src/services/api.js
@@ -1,17 +1,40 @@
-import axios from 'axios';
-import ENDPOINT from '../constants';
+// import axios from 'axios';
+// import ENDPOINT from '../constants';
+
+// const api = axios.create({
+//   baseURL: ENDPOINT
+// });
+
+// api.interceptors.request.use((config) => {
+
+//   const token = localStorage.getItem('accessToken');
+//   if (token) {
+//     config.headers.Authorization = `Bearer ${token}`;
+//   }
+//   return config;
+// }, (error) => Promise.reject(error));
+
+// export default api;
+import axios from "axios";
+import ENDPOINT from "../constants";
 
 const api = axios.create({
-  baseURL: ENDPOINT
+  baseURL: ENDPOINT,
+  withCredentials: false, // ⭐ Render does NOT accept cookies
 });
 
-api.interceptors.request.use((config) => {
-
-  const token = localStorage.getItem('accessToken');
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-  return config;
-}, (error) => Promise.reject(error));
+// ⭐ Always attach token manually
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("accessToken");
+    if (token) {
+      config.headers["Authorization"] = `Bearer ${token}`;
+    } else {
+      console.warn("⚠ No token found in localStorage");
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
 
 export default api;
